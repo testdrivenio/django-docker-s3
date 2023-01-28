94447bcccc11a25d9fd8ff35762e7570676f8179
Michael Herman <hermanmu@gmail.com>
2023-01-28 12:15:29 -0600

    updates

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -129,3 +129,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:1337', 'http://localhost:1337']
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
